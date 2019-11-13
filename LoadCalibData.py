7a340180a99033ca5a6482cf600c656e08df6789
Ben D <33376747+BenD@users.noreply.github.com>
2019-11-13 20:37:34 +0000

    finsihed calibrateCamera3D method

--- LoadCalibData.py
+++ LoadCalibData.py
@@ -5,15 +5,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
+def calibrateCamera3D(data):
+    world_points = data[:, :3]  # Gets the first 3 points (x, y, z) of real world
+    image_points = data[:, 3:]  # Gets the last two (X,Y) of 2d world
+    A = []
+
+    for i in range(len(data)):
+        worldX = world_points[i][0]
+        worldY = world_points[i][1]
+        worldZ = world_points[i][2]
+
+        imageX = image_points[i][0]
+        imageY = image_points[i][1]
+
+        X = [worldX, worldY, worldZ, 1, 0, 0, 0, 0, -imageX * worldX, -imageX * worldY, -imageX * worldZ, -imageX]
+        Y = [0, 0, 0, 0, worldX, worldY, worldZ, 1, -imageY * worldX, -imageY * worldY, -imageY * worldZ, -imageY]
+
+        A.append(X)
+        A.append(Y)
+
+    A = np.asarray(A)
+    D, V = np.linalg.eig(A.transpose().dot(A))
+
+    est = V[:, np.argmin(D)]
+    print(est)
+
 data = np.loadtxt('data.txt')
 
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-ax.plot(data[:,0], data[:,1], data[:,2],'k.')
+# fig = plt.figure()
+# ax = fig.gca(projection="3d")
+# ax.plot(data[:,0], data[:,1], data[:,2],'k.')
+#
+# fig = plt.figure()
+# ax = fig.gca()
+# ax.plot(data[:,3], data[:,4],'r.')
 
-fig = plt.figure()
-ax = fig.gca()
-ax.plot(data[:,3], data[:,4],'r.')
-
-plt.show()
+# plt.show()
+calibrateCamera3D(data)
 
