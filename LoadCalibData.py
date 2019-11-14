868be8baeddca176a0317d3e413da6596360f1a4
Ben D <33376747+BenD@users.noreply.github.com>
2019-11-14 16:30:38 +0000

    Added show camera functionality, need to change a few lines (indicated by comments in code)

--- LoadCalibData.py
+++ LoadCalibData.py
@@ -28,18 +28,44 @@ def calibrateCamera3D(data):
     D, V = np.linalg.eig(A.transpose().dot(A))
 
     est = V[:, np.argmin(D)]
-    print(est)
+    est = np.reshape(est, (3,4))
+    return est
+    # print(est)
+
+def visualiseCameraCalibration3D(data, P):
+    threeD_homogenous = np.append(data[:, :3],np.ones([len(data),1]),1)
+    twoD_homogenous = np.append(data[:, 3:], np.ones([len(data), 1]), 1)
+
+    """
+    Get the re-projection matrix by getting the dot product of the Camera
+    matrix, P, and the transpose of the 3D points, then transposing the
+    result.
+    """
+
+    '''
+    [ 8.65064148e-03  1.07863245e-03 -3.87162696e-03  9.98554504e-01
+  6.27447267e-05  9.18884379e-03  4.63678386e-04 -5.20172279e-02
+  6.21374751e-06  2.17125723e-06  6.39659086e-06  2.73310960e-03]
+
+    '''
+
+    # print(threeD_homogenous.shape, P.shape)
+    # quit()
+    threeD_homogenous = P.dot(threeD_homogenous.transpose())
+    final_points = threeD_homogenous.transpose()
+
+    # need to change below here
+    final_points[:,0] = final_points[:,0] / final_points[:,2] # X = X/Z
+    final_points[:,1] = final_points[:,1] / final_points[:,2] # Y = Y/Z
+
+    # Plot the re-projected 2D points.
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.plot(final_points[:,0], final_points[:,1], 'g.')
+    # The reprojection againest the old 2D points.
+    ax.plot(data[:,3], data[:,4],'r.')
+    plt.show()
 
 data = np.loadtxt('data.txt')
-
-# fig = plt.figure()
-# ax = fig.gca(projection="3d")
-# ax.plot(data[:,0], data[:,1], data[:,2],'k.')
-#
-# fig = plt.figure()
-# ax = fig.gca()
-# ax.plot(data[:,3], data[:,4],'r.')
-
-# plt.show()
-calibrateCamera3D(data)
-
+P = calibrateCamera3D(data)
+visualiseCameraCalibration3D(data,P)
