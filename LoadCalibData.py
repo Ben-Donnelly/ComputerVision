2f70e26e455b48094f353244de7c05b2324797aa
Ben D <33376747+BenD@users.noreply.github.com>
2019-11-15 14:02:25 +0000

    finished visualise

--- LoadCalibData.py
+++ LoadCalibData.py
@@ -21,49 +21,47 @@ def calibrateCamera3D(data):
         X = [worldX, worldY, worldZ, 1, 0, 0, 0, 0, -imageX * worldX, -imageX * worldY, -imageX * worldZ, -imageX]
         Y = [0, 0, 0, 0, worldX, worldY, worldZ, 1, -imageY * worldX, -imageY * worldY, -imageY * worldZ, -imageY]
 
-        A.append(X)
-        A.append(Y)
+        A.append((X,Y))
 
     A = np.asarray(A)
+    A = np.reshape(A, (982, 12)) # If getting error may need to seperate append above
+
     D, V = np.linalg.eig(A.transpose().dot(A))
 
     est = V[:, np.argmin(D)]
-    est = np.reshape(est, (3,4))
+
+    est = np.reshape(est, (3,4)) # (12,) -> (3,4) for transpose below
     return est
     # print(est)
 
 def visualiseCameraCalibration3D(data, P):
     threeD_homogenous = np.append(data[:, :3],np.ones([len(data),1]),1)
-    twoD_homogenous = np.append(data[:, 3:], np.ones([len(data), 1]), 1)
-
+    # twoD_homogenous = np.append(data[:, 3:], np.ones([len(data), 1]), 1)
+    image_points = data[:, 3:]
     """
     Get the re-projection matrix by getting the dot product of the Camera
     matrix, P, and the transpose of the 3D points, then transposing the
     result.
     """
 
-    '''
-    [ 8.65064148e-03  1.07863245e-03 -3.87162696e-03  9.98554504e-01
-  6.27447267e-05  9.18884379e-03  4.63678386e-04 -5.20172279e-02
-  6.21374751e-06  2.17125723e-06  6.39659086e-06  2.73310960e-03]
-
-    '''
 
     # print(threeD_homogenous.shape, P.shape)
     # quit()
     threeD_homogenous = P.dot(threeD_homogenous.transpose())
-    final_points = threeD_homogenous.transpose()
+    overall = threeD_homogenous.transpose()
 
     # need to change below here
-    final_points[:,0] = final_points[:,0] / final_points[:,2] # X = X/Z
-    final_points[:,1] = final_points[:,1] / final_points[:,2] # Y = Y/Z
+
+    overall[:,0] /= overall[:,2] # X = X/Z
+    overall[:,1] /= overall[:,2] # Y = Y/Z
 
     # Plot the re-projected 2D points.
-    fig = plt.figure()
-    ax = fig.gca()
-    ax.plot(final_points[:,0], final_points[:,1], 'g.')
+    fig = plt.figure("Camera calibration", figsize=(50, 50))
+    ax = fig.gca() # Automatic points
+    ax.plot(overall[:,0], overall[:,1], 'b.')
     # The reprojection againest the old 2D points.
-    ax.plot(data[:,3], data[:,4],'r.')
+
+    ax.plot(image_points[:, 0],  image_points[:, 1], 'r.') # could just use image points
     plt.show()
 
 data = np.loadtxt('data.txt')
